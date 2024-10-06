99a7409f75de83b18cefad599a0e1a99aff0f696
Iqbal Zayyan <73063960+iqbalzayn01@users.noreply.github.com>
2024-10-06 17:11:39 +0700

    update: fix mongodb connected

--- server-side/app/db/index.js
+++ server-side/app/db/index.js
@@ -1,11 +1,35 @@
+// const mongoose = require('mongoose');
+// const { urlDb } = require('../config');
+
+// mongoose
+//   .connect(urlDb)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch((error) => console.log(error));
+
+// const db = mongoose.connection;
+
+// module.exports = db;
+
 const mongoose = require('mongoose');
 const { urlDb } = require('../config');
 
 mongoose
-  .connect(urlDb)
+  .connect(urlDb, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+  })
   .then(() => console.log('MongoDB Connected'))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log('MongoDB Connection Error:', error));
 
 const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+});
 
 module.exports = db;
